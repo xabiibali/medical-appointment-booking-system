0cc193e4449e34ac05f81b6b6c8d1c1f0902bfdc
xabiib-ali <you@example.com>
2023-11-26 05:40:37 -0800

    fixing

--- api/server.js
+++ api/server.js
@@ -1,12 +1,12 @@
 import express from 'express'
-import {json} from 'express'
+// import {json} from 'express'
 import doctor from './doctor.js'
 import patient from './patient.js'
 import patientHistory from './patientHistory.js'
 import appointment from './appointment.js'
 const server = express()
 
-server.use(json())
+server.use(express.json())
 
 server.use("/api/doctor", doctor)
 server.use("/api/patient", patient)
